023fddb0a791f28b19fa0216bc4d68690e565787
John Hawkins <littlehawk93@users.noreply.github.com>
2023-11-21 17:25:39 -0600

    added device screen dimensions and other warnings to image upload page

--- views/site/device.php
+++ views/site/device.php
@@ -1,14 +1,16 @@
 <?php
 
 /** @var yii\web\View $this */
+/** @var app\models\Device $device */
 /** @var bool $success */
 
 $this->title = "Paradisaea | Upload Image";
 ?>
 <h4>Upload an Image</h4>
 <form class="mt-4" action="<?= '/api/device/' . $device->view_id . '/image?redirect' ?>" method="POST" enctype="multipart/form-data">
-    <label>Select an image file to upload: </label>
+    <label>Select an image file to upload (JPG or PNG): </label>
     <input type="file" name="file" id="id" accept=".jpg,.png,image/jpg,image/jpeg,image/png" /><br />
+    <label class="font-italic">Max 1 MB. Note: resize image to <?= $device->width ?>x<?= $device->height ?> px before uploading to maximize image quality.</label>
     <button type="submit" class="btn btn-outline-dark mt-2">Upload Image</button>
 </form>
 <?php if ($success): ?>
